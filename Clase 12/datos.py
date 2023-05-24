071e0fda2880af66635cc6e6b3aca27d7d002211
Danielsaan9 <134543103+Danielsaan9@users.noreply.github.com>
2023-05-24 14:34:04 -0400

    Archivos datos.py sin validar

---
+++ Clase 12/datos.py
@@ -0,0 +1,8 @@
+#EJEMPLO DE USO  DE REPOSITORIO
+print("Datos personales")
+print("- - - - - - - - -")
+vNom = input("Ingrese su nombre : ")
+vEdad = input("Ingrese su edad : ")
+print(f"Su nombre es : {vNom}")
+print(f"Su edad es : {vEdad}")
+print("FIN PROGRAMA")
